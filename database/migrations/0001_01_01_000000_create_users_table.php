350acd6db3b6389d2fc3fcadd1bdda387503a5d4
R3ndly <akalinart020@gmail.com>
2025-07-04 23:08:59 +0800

    Доделал миграции

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,20 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-*/
     public function up(): void
     {
-       /* Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });*/
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('email', 255)->unique();
+            $table->string('password', 255);
+            $table->foreignId('gender_id')->nullable()->constrained('gender');
+            $table->timestamp('created_at')->useCurrent();
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('email', 255)->unique();
+            $table->string('password', 255);
+            $table->foreignId('gender_id')
+                  ->nullable()
+                  ->constrained('gender')
+                  ->onUpdate('cascade')
+                  ->onDelete('set null');
+            $table->timestamp('created_at')->useCurrent();
+        });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
@@ -37,9 +44,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
